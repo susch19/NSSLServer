8bffb58067fd47b3bba2c8c312912b8d281764e5
Sascha <suschi@ist-einmalig.de>
2022-07-03 13:32:25 +0200

    Only use  nuget references for lokal release builds (#5)
    * as the CI needs the projects references for the correct NuGet pack and local debug should also include the project reference for debuggability
    * Fix date time now not being correct for Postgres in the newest version as all entries in the db should be UTC date time

--- NSSLServer.Plugin.Shoppinglist/Plugin.cs
+++ NSSLServer.Plugin.Shoppinglist/Plugin.cs
@@ -21,9 +21,11 @@ namespace NSSLServer.Plugin.Shoppinglist
     public class Plugin : IPlugin
     {
         public string Name { get; } = "Plugin Shoppinglist";
+        public static Logger Logger { get; private set; }
 
         public bool Initialize(LogFactory logFactory)
         {
+            Logger = logFactory.GetCurrentClassLogger();
             if (File.Exists("external/service_account.json"))
             {
                 FirebaseApp.Create(new AppOptions { Credential = Google.Apis.Auth.OAuth2.GoogleCredential.FromFile("external/service_account.json") });
