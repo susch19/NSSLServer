1b4c8996eec25aa373b9a79c5cdeab7777b87d4d
susch19 <suschi@ist-einmalig.de>
2022-07-10 14:03:14 +0200

    Added a new plugin for web app hosting
    * this plugin allows hosting a seperate web app as a sub path, which will be used to run nssl on the server, if possible

---
+++ NSSLServer.Plugin.Webapp/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+using NLog;
+
+using NSSLServer.Core.Extension;
+
+using System.IO;
+
+namespace NSSLServer.Plugin.Webapp
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "WebappPlugin";
+
+        internal static Logger Logger { get; private set; }
+
+        public bool Initialize(LogFactory factory)
+        {
+            Logger = factory.GetCurrentClassLogger();
+
+            return true;
+        }
+
+        void IPlugin.Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            app.UseFileServer(new FileServerOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(environment.ContentRootPath, "Static", "web")),
+                RequestPath = new PathString("/webapp")
+            });
+        }
+
+    }
+}
